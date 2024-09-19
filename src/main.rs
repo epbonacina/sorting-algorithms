67c1aed06a9f54aeafacc157b048b4d1c79573af
Enzo Pedro Bonacina <epbonacina@gmail.com>
2024-09-19 15:36:04 -0300

    A little refactoring section in main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,42 +8,52 @@ fn benchmark(sort_fn: fn(&mut [u32]), array: &mut [u32], sort_name: &str) {
     let start = Instant::now();
     sort_fn(array);
     let duration = start.elapsed();
-    // println!("{:?} sorted array: {:?}", sort_name, array);
-    println!("{} took: {:?}", sort_name, duration);
-    println!("============================");
+    let result = if is_sorted(array) {
+        "APPROVED"
+    } else {
+        "REJECTED"
+    };
+    println!("[{}] {} took: {:?}", result, sort_name, duration);
+}
+
+fn is_sorted(array: &[u32]) -> bool {
+    let mut prev = 0;
+    for &value in array {
+        if value < prev {
+            return false;
+        }
+        prev = value;
+    }
+    true
 }
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let mut base_array = vec![0; 1_000_000];
-    for i in 0..base_array.len() {
-        base_array[i] = rng.gen_range(0..=100);
+    let mut array = [0; 100_000];
+    for i in 0..array.len() {
+        array[i] = rng.gen_range(0..=100);
     }
-    // base_array = [47, 24, 22, 55, 80, 92, 99, 8, 0, 31];
-    // println!("Base array: {base_array:?}");
-    println!("============================");
 
-    let mut array = base_array.clone();
-    benchmark(heap_sort::heap_sort, &mut array, "Heap Sort");
+    let algorithms: &[(fn(&mut [u32]), &str)] = &[
+        (heap_sort::heap_sort, "Heap sort"),
+        (sorting::merge_sort::merge_sort, "Merge sort"),
+        (sorting::bucket_sort::bucket_sort, "Bucket sort"),
+        (sorting::counting_sort::counting_sort, "Counting sort"),
+        (sorting::quick_sort::hoares_quick_sort, "Hoare's quick sort"),
+        (
+            sorting::quick_sort::lomutos_quick_sort,
+            "Lomuto's quick sort",
+        ),
+        (sorting::insertion_sort::insertion_sort, "Insertion sort"),
+        (sorting::selection_sort::selection_sort, "Selection sort"),
+    ];
 
-    let mut array = base_array.clone();
-    benchmark(sorting::merge_sort::merge_sort, &mut array, "Merge Sort");
+    for (algo, algo_name) in algorithms {
+        benchmark(*algo, &mut array.clone(), algo_name)
+    }
 
-    let mut array = base_array.clone();
-    benchmark(sorting::bucket_sort::bucket_sort, &mut array, "Bucket Sort");
-
-    let mut array = base_array.clone();
-    benchmark(sorting::counting_sort::counting_sort, &mut array, "Counting Sort");
-
-    let mut array = base_array.clone();
-    benchmark(sorting::quick_sort::hoares_quick_sort, &mut array, "Hoare's Quick Sort");
-
-    let mut array = base_array.clone();
-    benchmark(sorting::quick_sort::lomutos_quick_sort, &mut array, "Lomuto's Quick Sort");
-
-    let mut array = base_array.clone();
-    benchmark(sorting::insertion_sort::insertion_sort, &mut array, "Insertion Sort");
-
-    let mut array = base_array.clone();
-    benchmark(sorting::selection_sort::selection_sort, &mut array, "Selection Sort");
+    let start = Instant::now();
+    array.sort();
+    let duration = start.elapsed();
+    println!("The standard sorting algorithm took {:?}", duration);
 }
